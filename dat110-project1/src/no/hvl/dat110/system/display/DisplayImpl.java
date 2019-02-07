148bf2f7b46904b6051a9c75677347028a14aa6b
h151274 <bonn_96aa@hotmail.com>
2019-02-07 14:41:21 +0100

    display done

--- dat110-project1/src/no/hvl/dat110/system/display/DisplayImpl.java
+++ dat110-project1/src/no/hvl/dat110/system/display/DisplayImpl.java
@@ -5,23 +5,30 @@ import no.hvl.dat110.rpc.RPCUtils;
 
 public class DisplayImpl implements RPCImpl {
 
-	public void write(String message) {
-		System.out.println("DISPLAY:" + message);
-	}
-	
-	public byte[] invoke(byte[] request) {
-		
-		byte[] reply;
-		byte rpcid;
-		
-		// TODO: 
-		// implement unmarshalling, call, and marshall for write RPC method
-		// look at how this is done int he SensorImpl for the read method
-		
-		if (true) {
-			  throw new RuntimeException("not yet implemented");
-		}
-		
-		return reply;
-	}
+	 public void write(String message) {
+	        System.out.println("DISPLAY:" + message);
+	    }
+
+	    public byte[] invoke(byte[] request) {
+
+	        byte rpcid;
+	        byte[] reply;
+
+	        // TODO
+	        // implement unmarshalling, call, and marshall for write RPC method
+	        // look at how this is done int he SensorImpl for the read method
+
+	        //Unmarshall request into string
+	        String message = RPCUtils.unmarshallString(request);
+
+	        //Write message to console
+	        write(message);
+
+	        //Marshall message back into an reply
+	        rpcid = request[0];
+	        reply = RPCUtils.marshallString(rpcid, message);
+
+	        return reply;
+	    }
+
 }
